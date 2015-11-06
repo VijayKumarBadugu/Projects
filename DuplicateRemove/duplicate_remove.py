0076875c5e5d2a46867755d282c223537e377b8b
Vijay Kumar Badugu <badugu.vijay@gmail.com>
2015-11-05 21:36:45 -0500

    duplicate remove added

---
+++ DuplicateRemove/duplicate_remove.py
@@ -0,0 +1,26 @@
+# don't use any library functions
+# don't use any temp data structures
+# remove the duplicates in the in_arr 
+in_arr = [1,2,3,4,5,2,2,33,4,5]
+#in_arr = [1,2,3,4,5,33]
+
+
+for i in range(1,len(in_arr)):
+	j=i+1
+	while(j<len(in_arr)):
+		
+		
+		if j==len(in_arr):
+			break
+
+		if(in_arr[i]==in_arr[j]):
+			
+			
+			in_arr = in_arr[0:j]+in_arr[j+1:len(in_arr)]
+			j=j-1
+		j=j+1
+print in_arr
+			
+			
+		
+	
