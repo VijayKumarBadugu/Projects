93117cd98c0c149c993900dcb4364c4e3dae92da
Vijay Kumar Badugu <badugu.vijay@gmail.com>
2015-11-10 14:39:36 -0500

    condition refined in improved variant

--- DuplicateRemove/improved.py
+++ DuplicateRemove/improved.py
@@ -1,7 +1,7 @@
 # don't use any library functions
 # don't use any temp data structures
 # remove the duplicates in the in_arr
-#in_arr = [2,2,2,2,2,2,2,2] 
+#in_arr = [2,2,2,2,2,2,2,2,3,3,3,3,5,5,5,5,3,3,3,3] 
 in_arr = [1,2,3,4,5,2,2,33,4,5]
 #in_arr = [1,2,3,4,5,33]
 #performs in n2
@@ -11,7 +11,7 @@ i	=	1
 LEN	=	len(in_arr)
 while(i<LEN):
 	status	= 0
-	for j in range(0,i):
+	for j in range(0,count):
 		if(in_arr[i]==in_arr[j]):
 			status	=	1
 	if(status==0):
